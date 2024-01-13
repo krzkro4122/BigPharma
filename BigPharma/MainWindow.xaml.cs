914b7b60fa271abcee0d44b9b18254999f46e470
krzkro4122 <krolkrzysztof1999@gmail.com>
2024-01-13 17:47:15 +0100

    shutdown-related changes

--- BigPharma/MainWindow.xaml.cs
+++ BigPharma/MainWindow.xaml.cs
@@ -1,18 +1,5 @@
-﻿using Microsoft.VisualBasic;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace BigPharma
 {
@@ -34,7 +21,9 @@ namespace BigPharma
 
         private void OnClosing(object sender, CancelEventArgs e)
         {
-            foreach(Window window in Application.Current.Windows) window.Close();
+            authWindow.Close();
+            stock.Close();
+            sale.Close();
         }
 
         public void Unlock_Resources()
