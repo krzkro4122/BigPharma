de9cbd23f943e3fe4328aa3a380801f7fe7afb74
krzkro4122 <krolkrzysztof1999@gmail.com>
2024-01-26 11:49:53 +0100

    strechable order browser, nullable transaction list

--- BigPharmaEngine/Models/TransactionModel.cs
+++ BigPharmaEngine/Models/TransactionModel.cs
@@ -3,6 +3,6 @@ namespace BigPharmaEngine.Models;
 public class TransactionModel
 {
     public int Id { get; set; }
-    public List<OrderModel> OrderModels { get; set; }
+    public List<OrderModel>? OrderModels { get; set; }
     public DateTime? CompletionDate { get; set; }
 }
